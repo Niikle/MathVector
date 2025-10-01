71752bf355099930fcf2e88aea95a79bd8111ac3
Niikle <qwerty>
2025-10-01 22:31:22 +0300

    add 2 constructors and fix bag

--- MathVector.cpp
+++ MathVector.cpp
@@ -6,7 +6,7 @@ MathVector::MathVector() {
 	this->startPoint.z =
 	this->endPoint.x =
 	this->endPoint.y = 
-	this->endPoint.z = i = j = 0;
+	this->endPoint.z = i = j = k = 0;
 }
 
 MathVector::MathVector(Point startPoint, Point endPoint) {
@@ -16,7 +16,8 @@ MathVector::MathVector(Point startPoint, Point endPoint) {
 	this->endPoint.x = endPoint.x;
 	this->endPoint.y = endPoint.y;
 	this->endPoint.z = endPoint.z;
-	calculateIJ();
+
+	calculateIJK();
 }
 
 MathVector::MathVector(MathVector& other) {
@@ -28,11 +29,35 @@ MathVector::MathVector(MathVector& other) {
 	this->endPoint.z = other.endPoint.z;
 	this->i = other.i;
 	this->j = other.j;
+	this->k = other.k;
 }
 
-void MathVector::calculateIJ() {
+MathVector::MathVector(double x1, double y1, double z1, 
+					   double x2, double y2, double z2) {
+	this->startPoint.x = x1;
+	this->startPoint.y = y1;
+	this->startPoint.z = z1;
+	this->endPoint.x = x2;
+	this->endPoint.y = y2;
+	this->endPoint.z = z2;
+	calculateIJK();
+}
+
+MathVector::MathVector(double i, double j, double k) {
+	this->startPoint.x =
+	this->startPoint.y =
+	this->startPoint.z = 0;
+	this->endPoint.x = i;
+	this->endPoint.y = j;
+	this->endPoint.z = k;
+
+	calculateIJK();
+}
+
+void MathVector::calculateIJK() {
 	i = endPoint.x - startPoint.x;
-	j = endPoint.y - endPoint.y;
+	j = endPoint.y - startPoint.y;
+	k = endPoint.z - startPoint.z;
 }
 
 bool MathVector::operator == (const MathVector& other) {
@@ -49,5 +74,21 @@ MathVector& MathVector::operator * (double num) {
 	this->endPoint.x *= num;
 	this->endPoint.y *= num;
 	this->endPoint.z *= num;
-	calculateIJ();
+
+	calculateIJK();
+
+	return *this;
+}
+
+MathVector& MathVector::operator + (const MathVector& other) {
+	this->startPoint.x += other.startPoint.x;
+	this->startPoint.y += other.startPoint.y;
+	this->startPoint.z += other.startPoint.z;
+	this->endPoint.x += other.endPoint.x;
+	this->endPoint.y += other.endPoint.y;
+	this->endPoint.z += other.endPoint.z;
+
+	calculateIJK();
+
+	return *this;
 }
--- MathVector.h
+++ MathVector.h
@@ -4,18 +4,23 @@
 class MathVector{
 private:
 	Point startPoint, endPoint;
-	double i, j;
+	double i, j, k;
+
 private:
-	void calculateIJ();
+	void calculateIJK();
 
 public:
 	MathVector();
 	MathVector(Point startPoint, Point endPoint);
 	MathVector(MathVector& other);
+	MathVector(double i, double j, double k);
+	MathVector(double x1, double y1, double z1, 
+			   double x2, double y2, double z2);
 
 	bool operator == (const MathVector& other);
 	bool operator != (const MathVector& other);
 
 	MathVector& operator * (double num);
+	MathVector& operator + (const MathVector& other);
 
 };
